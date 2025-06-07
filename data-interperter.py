f48f493789fc0721611355aaa5a99787d4c56004
deb <hxin.radin@gmail.com>
2025-06-07 21:30:25 +0200

    Finished data interperter, data.xlsx

--- data-interperter.py
+++ data-interperter.py
@@ -1,4 +1,5 @@
 import xlsxwriter
+from datetime import datetime
 import json
 import os
 
@@ -11,17 +12,35 @@ files = []
 for i in files_raw:
     if i.split("-")[0] == "run":
         files.append(i)
-
-c = 0
+files.sort()
+treatment_c = 0
 for i in files:
-    treatment =  eval(i.split("-")[1][0])
     with open(f"./data/{i}","r") as file:
         data_raw = json.load(file)
     data_raw = list(data_raw)
+    i = i.split(".")[0]
+    treatment =  i.split("-",1)[1]
     timestart = data_raw[0]["start time"]
     timeend = data_raw[1]["end time"]
-    worksheet.write(1,c,f"start: {timestart} end: {timeend} delta: {timeend-timestart}")
-    c = c + 5
+    delta_temp = eval(data_raw[len(data_raw)-1]["avg"])-eval(data_raw[2]["avg"])
+    delta_temp = round(delta_temp,2)
+    
+    for j in range(2):
+        data_raw.pop(0)
+
+    worksheet.write(0,treatment_c,f"{treatment} ")
+    worksheet.write(1,treatment_c,f"{datetime.strptime(timeend , '%H:%M:%S')-datetime.strptime(timestart,'%H:%M:%S')}")
+    worksheet.write(2,treatment_c,f"DT: {delta_temp}")
+    
+    
+    
+    for temp_c in range(3,len(data_raw)):
+        worksheet.write(temp_c,treatment_c,data_raw[temp_c]["avg"])
+    
+
+    treatment_c = treatment_c + 1
+
+workbook.close()
 
 
 
